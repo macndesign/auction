b94f0dff6d9e6aade54f5be62863602d1823941d
Mário Chaves <macndesign@gmail.com>
2016-02-20 00:57:58 -0200

    initial react

---
+++ README.md
@@ -0,0 +1,23 @@
+# Auction
+
+A simple project to learn ReactJS
+
+## I'll beginning with a drawing
+
+![sample](dep/draw.jpg)
+
+## Then a wireframe
+
+![sample](dep/wireframe.png)
+
+## Finally into the browser
+
+![sample](dep/browser.png)
+
+
+```
+(\____/)
+( ͡ ⚫͜ ʖ͡⚫
+\╭☞ \╭☞
+```
+Pah!!
---
+++ src/components/Main.js
@@ -0,0 +1,73 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let foxImage = require('../images/fox.jpg');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div id="wrapper">
+        <div id="header">
+          <h1>VW - Fox 1.6 Prime GII Flex - 2011/2012 - Prata</h1>
+          <h2>Início: 20/02/2016 às 10:00h - Lote: 00123</h2>
+        </div>
+        <div id="container">
+          <div id="main">
+            <div id="photo">
+              <img src={foxImage} alt="VW Fox" />
+            </div>
+            <div id="section">
+              <div id="bid">
+                <h3>
+                  Lance atual<br />
+                  <small>R$</small>
+                  <strong>14.500,00</strong>
+                </h3>
+                <h4>João Constantino</h4>
+              </div>
+              <div id="bid-more">
+                <h4>Total de 17 lances</h4>
+                <label for="user">Usuário</label>
+                <strong id="user">Platéia</strong>
+                <br />
+                <label for="city">Cidade</label>
+                <strong id="city">Fortaleza</strong>
+                <br />
+                <label for="send">Enviado às</label>
+                <strong id="send">11:17:33</strong>
+              </div>
+            </div>
+          </div>
+          <div id="aside">
+            <div id="info">
+              <label for="principal">Comitente</label>
+              <strong id="principal">Banco Bradesco S/A</strong>
+              <hr />
+              <label for="evaluation">Avaliação</label>
+              <strong id="evaluation">R$ 12.100,00</strong>
+              <hr />
+              <label for="starting">Lance inicial</label>
+              <strong id="starting">R$ 6.000,00</strong>
+              <hr />
+              <label for="increasing">Incremento</label>
+              <strong id="increasing">R$ 500,00</strong>
+              <hr />
+            </div>
+            <div id="control">
+              <h4><em>Contagem regressiva</em></h4>
+              <div id="clock">00:42</div>
+              <button>Ofertar lance!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
